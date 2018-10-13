f25fc2a4edb2963085103022519ce0312d0a3d96
Maxim Salnikov <salnikov@gmail.com>
2018-10-13 04:46:17 -0400

    push added

--- src/app/push-subscription/push-subscription.component.ts
+++ src/app/push-subscription/push-subscription.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material';
+import { SwPush } from '@angular/service-worker';
 
 import { ConfigService } from './../config.service';
 import { PushSubscriptionService } from './../push-subscription.service';
 
 import { take } from 'rxjs/operators';
-
-// Import SwPush here
 
 @Component({
   selector: 'push-subscription',
@@ -19,6 +18,7 @@ export class PushSubscriptionComponent implements OnInit {
   private snackBarDuration: number = 2000;
 
   constructor(
+    private swPush: SwPush,
     private pushSubscriptionService: PushSubscriptionService,
     public snackBar: MatSnackBar,
     private configService: ConfigService
@@ -29,14 +29,71 @@ export class PushSubscriptionComponent implements OnInit {
   }
 
   subscribeToPush() {
-    // Code to subscribe user to the Push notifications
+    this.swPush
+      .requestSubscription({
+        serverPublicKey: this.VAPID_PUBLIC_KEY
+      })
+      .then(pushSubscription => {
+        this.pushSubscriptionService.addSubscriber(pushSubscription).subscribe(
+          res => {
+            console.log('[Push Subscription] Add subscriber request answer', res);
+
+            let snackBarRef = this.snackBar.open(
+              'Now you are subscribed',
+              null,
+              {
+                duration: this.snackBarDuration
+              }
+            );
+          },
+          err => {
+            console.log('[Push Subscription] Add subscriber request failed', err);
+          }
+        );
+      })
+      .catch(err => {
+        console.error(err);
+      });
   }
 
   unsubscribeFromPush() {
-    // Code to unsubscribe user from the Push notifications
+    // Get active subscription
+    this.swPush.subscription.pipe(take(1)).subscribe(pushSubscription => {
+      console.log('[Push Subscription] pushSubscription', pushSubscription);
+
+      // Delete the subscription on the backend
+      this.pushSubscriptionService.deleteSubscriber(pushSubscription).subscribe(
+        res => {
+          console.log('[Push Subscription] Delete subscriber request answer', res);
+
+          let snackBarRef = this.snackBar.open(
+            'Now you are unsubscribed',
+            null,
+            {
+              duration: this.snackBarDuration
+            }
+          );
+
+          // Unsubscribe current client (browser)
+          pushSubscription
+            .unsubscribe()
+            .then(success => {
+              console.log('[Push Subscription] Unsubscription successful', success);
+            })
+            .catch(err => {
+              console.log('[Push Subscription] Unsubscription failed', err);
+            });
+        },
+        err => {
+          console.log('[Push Subscription] Delete subscription request failed', err);
+        }
+      );
+    });
   }
 
   showMessages() {
-    // Code to listen to the Push notifications
+    this.swPush.messages.subscribe(message => {
+      console.log(message);
+    });
   }
 }
