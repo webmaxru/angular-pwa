19e9b6675e5af0dc5b8f653b7110e7276ecfe1fb
Maxim Salnikov <salnikov@gmail.com>
2018-10-15 08:06:12 +0200

    a2hs code added

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import {
+  BreakpointObserver,
+  Breakpoints,
+  BreakpointState
+} from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -9,12 +13,37 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent {
+  installPromptEvent;
+  btnInstallDisabled = true;
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches)
-    );
-    
-  constructor(private breakpointObserver: BreakpointObserver) {}
-  
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(map(result => result.matches));
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    window.addEventListener('beforeinstallprompt', event => {
+      // Prevent Chrome <= 67 from automatically showing the prompt
+      event.preventDefault();
+      // Stash the event so it can be triggered later.
+      this.installPromptEvent = event;
+      // Update the install UI to notify the user app can be installed
+      this.btnInstallDisabled = false;
+    });
   }
+
+  install() {
+    this.btnInstallDisabled = true;
+    // Show the modal add to home screen dialog
+    this.installPromptEvent.prompt();
+    // Wait for the user to respond to the prompt
+    this.installPromptEvent.userChoice.then(choice => {
+      if (choice.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      // Clear the saved prompt since it can't be used again
+      this.installPromptEvent = null;
+    });
+  }
+}
