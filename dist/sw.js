a69b15eb3512f48767855b932d7344223bc8296b
Maxim Salnikov <salnikov@gmail.com>
2019-05-15 02:09:30 +0100

    api err

--- dist/sw.js
+++ dist/sw.js
@@ -39,11 +39,11 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "da84d7f973db1f08e3e88e1cf8f73f1d"
+    "revision": "a71f23df8a4bd7e89d4a19a7b500a9c4"
   },
   {
-    "url": "main.ec460fd130b082fe2ebf.js",
-    "revision": "0cd2f6d6084bf627a122e1b1f0c3f9ac"
+    "url": "main.97a3068a07010b8662e7.js",
+    "revision": "4989176ab0bda518ee0fa8b13b106383"
   },
   {
     "url": "polyfills.0fa3e9588b07b25d43c2.js",
