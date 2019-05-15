29378e9e0614db4060d089e4a036fd10f1ed7aee
Maxim Salnikov <salnikov@gmail.com>
2019-05-15 02:18:26 +0100

    api err

--- dist/sw.js
+++ dist/sw.js
@@ -26,6 +26,38 @@ workbox.precaching.precacheAndRoute([
     "revision": "0bc833d0d9232ee71e490b997452c182"
   },
   {
+    "url": "assets/icons/icon-128x128.png",
+    "revision": "ef83aae951233f27af90648fa8b825c9"
+  },
+  {
+    "url": "assets/icons/icon-144x144.png",
+    "revision": "a455cb88bc9c224bb416401b605a4570"
+  },
+  {
+    "url": "assets/icons/icon-152x152.png",
+    "revision": "18559b786ff99db4a9c342c3a111ca90"
+  },
+  {
+    "url": "assets/icons/icon-192x192.png",
+    "revision": "7534103c22896094bbbed55c5a37269e"
+  },
+  {
+    "url": "assets/icons/icon-384x384.png",
+    "revision": "53e898b42719b989fe858571fd2263ab"
+  },
+  {
+    "url": "assets/icons/icon-512x512.png",
+    "revision": "b997df4eaa26b0e8455d9de852284fd5"
+  },
+  {
+    "url": "assets/icons/icon-72x72.png",
+    "revision": "04cc09572092242152768a313432a0fa"
+  },
+  {
+    "url": "assets/icons/icon-96x96.png",
+    "revision": "9434e61a9e6eb19b1c588b06d7afd7e8"
+  },
+  {
     "url": "assets/images/logo.png",
     "revision": "f5fd664cc80a6c77d6c79e5bd2653426"
   },
@@ -35,11 +67,11 @@ workbox.precaching.precacheAndRoute([
   },
   {
     "url": "index.html",
-    "revision": "da84d7f973db1f08e3e88e1cf8f73f1d"
+    "revision": "532a1b138fd817ae4a3581a82213690b"
   },
   {
-    "url": "main.ec460fd130b082fe2ebf.js",
-    "revision": "0cd2f6d6084bf627a122e1b1f0c3f9ac"
+    "url": "main.97a3068a07010b8662e7.js",
+    "revision": "4989176ab0bda518ee0fa8b13b106383"
   },
   {
     "url": "polyfills.0fa3e9588b07b25d43c2.js",
