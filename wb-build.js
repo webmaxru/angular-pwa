832ba2867f4ccbebdcd8a2447fc55e59af68b4e9
Maxim Salnikov <salnikov@gmail.com>
2020-09-10 15:50:37 +0200

    pwa-test-1!

---
+++ wb-build.js
@@ -0,0 +1,19 @@
+
+const {injectManifest} = require('workbox-build')
+
+let workboxConfig = {
+  globDirectory: 'dist/angular-pwa',
+  globPatterns: [
+    'favicon.ico',
+    'index.html',
+    '*.css',
+    '*.js'
+  ],
+  swSrc: 'src/service-worker.js',
+  swDest: 'dist/angular-pwa/service-worker.js'
+}
+
+injectManifest(workboxConfig)
+  .then(({count, size}) => {
+    console.log(`Generated ${workboxConfig.swDest}, which will precache ${count} files, totaling ${size} bytes.`)
+  })
